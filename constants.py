1e5efcae77ed0ad01f0522e171036b2f03444a1a
kasyu04 <kasyu04@gmail.com>
2025-03-08 14:55:02 +0900

    first commit.

--- constants.py
+++ constants.py
@@ -1,3 +1,8 @@
 APP_NAME = "対話型商品レコメンド生成AIアプリ"
 AI_ICON_FILE_PATH = "images/ai_icon.jpg"
 USER_ICON_FILE_PATH = "images/user_icon.jpg"
+
+# 在庫状況の定数
+STOCK_STATUS_AVAILABLE = "あり"
+STOCK_STATUS_FEW_LEFT = "残りわずか"
+STOCK_STATUS_NONE = "なし"
